d347900e8d16a4312a9916f37bfb5b7e774e96c7
marinxx <127798969+marinxx@users.noreply.github.com>
2023-04-03 20:31:02 -0300

    Update teoremapitagoras.cpp

--- teoremapitagoras.cpp
+++ teoremapitagoras.cpp
@@ -1,27 +1,25 @@
 //calculo
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 //teorema de pitagoras 
 int main() {
     //componentes do calculo
-    float a, b, c, c2;
+    float cateto1, cateto2, hipotenusa;
     
     //medida de a
-    cout << "Digite o valor do cateto a: ";
-    cin >> a;
+    cout << "Digite o comprimento do cateto 1: ";
+    cin >> cateto1;
     //medida de b
-    cout << "Digite o valor do cateto b: ";
-    cin >> b;
+    cout << "Digite o comprimento do cateto 2: ";
+    cin >> cateto2;
     
     //formula para calcular o valor de c2
-    c2 = (a * a) + (b * b);
-    
-    //o valor de c
-    c = 
+    hipotenusa = sqrt(pow(cateto1, 2) + pow(cateto2, 2));
     
     //resultado obtido
-    cout << "A hipotenusa eh igual a: " << c << endl;
+    cout << "A hipotenusa eh igual a: " << hipotenusa << endl;
 
     return 0;
 }
